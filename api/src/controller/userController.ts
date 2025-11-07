1c85f01e108726a29b9b45cc7f363de80f098330
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-11-07 16:25:46 +0700

    create env.example

--- api/src/controller/userController.ts
+++ api/src/controller/userController.ts
@@ -55,57 +55,85 @@ export const userController = {
 
   // LOGIN
   async login(req: Request, res: Response) {
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-      return res.status(400).json({
-        message: "email dan password wajib diisi",
-        success: false
-      });
+    try {
+        const { email, password } = req.body;
+    
+        if (!email || !password) {
+          return res.status(400).json({
+            message: "email dan password wajib diisi",
+            success: false
+          });
+        }
+    
+        const { user, token } = await userService.loginUser(email, password);
+    
+        res.status(200).json({
+          message: "login berhasil",
+          success: true,
+          user,
+          token
+        });
+        
+    } catch (error: any) {
+        res.status(401).json({
+            message: error.message,
+            success: false
+        })
     }
-
-    const { user, token } = await userService.loginUser(email, password);
-
-    res.status(200).json({
-      message: "login berhasil",
-      success: true,
-      user,
-      token
-    });
   },
 
   // UPDATE user by ID
   async updateUser(req: Request, res: Response) {
-    const id = Number(req.params.id);
-    if (isNaN(id)) {
+
+    const idUpdate = Number(req.params.id);
+
+    if (isNaN(idUpdate)) {
       return res.status(400).json({
         message: "id tidak valid",
         success: false
       });
     }
 
-    const user = await userService.updatedUserById(id, req.body);
+    const currentUser = (req as any).user;
+
+    if(currentUser.role !== "Admin" && currentUser.id !== idUpdate) {
+        return res.status(403).json({
+            message: "akses ditolak",
+            success: false
+        });
+    }
+
+    const updatedUser = await userService.updatedUserById(idUpdate, req.body);
 
     res.status(200).json({
-      message: "user berhasil diperbarui",
-      success: true,
-      user
+        message: "user berhasil diperbarui",
+        success: true,
+        user: updatedUser
     });
   },
 
   // DELETE user by ID
   async deleteUser(req: Request, res: Response) {
 
-    const id = Number(req.params.id);
+    const idDelete = Number(req.params.id);
 
-    if (isNaN(id)) {
+    if (isNaN(idDelete)) {
       return res.status(400).json({
         message: "id tidak valid",
         success: false
       });
     }
 
-    await userService.deleteUserById(id);
+    const currentUser = (req as any).user;
+
+    if(currentUser.role !== "Admin") {
+        return res.status(403).json({
+            message: "akses ditolak",
+            success: false
+        });
+    }
+
+    await userService.deleteUserById(idDelete);
 
     res.status(200).json({
       message: "user berhasil dihapus",
@@ -117,7 +145,7 @@ export const userController = {
   async getProfile(req: Request, res: Response) {
 
     const currentUser = (req as any).user;
-    
+
     res.status(200).json({
       user: currentUser,
       success: true

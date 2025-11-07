5023cfc483d59a2dc4b0fbc22d2b64fce3cf7a06
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-11-07 14:41:26 +0700

    create handler and role middleware

--- api/src/routes/userRoutes.ts
+++ api/src/routes/userRoutes.ts
@@ -1,21 +1,26 @@
 import { Router } from "express";
 import { userController } from "../controller/userController";
 import { authMiddleware } from "../middleware/authMiddleware";
+import { authorizeRoles } from "../middleware/roleMiddleware";
+import { asyncHandler } from "../middleware/asyncHandler";
 
 const router = Router();
 
-router.get("/", userController.getAllUsers);
 
+//hanya admin yg bisa lihat user
+router.get("/", authMiddleware, authorizeRoles("Admin"), asyncHandler(userController.getAllUsers));
+
+// register & login (tidak butuh auth)
 router.post("/register", userController.register);
-
 router.post("/login", userController.login);
 
+//user bisa lihat profile
 router.get("/profile", authMiddleware, userController.getProfile);
 
+// GET, UPDATE, and DELETE by ID
 router.get("/:id", userController.getUserById);
-
 router.put("/:id", userController.updateUser);
-
 router.delete("/:id", userController.deleteUser);
+
 
 export default router;

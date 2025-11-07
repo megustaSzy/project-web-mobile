524c5591db08fb38f9c638570ce457d6d19f21ed
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-11-07 15:43:52 +0700

    update controller

--- api/src/routes/userRoutes.ts
+++ api/src/routes/userRoutes.ts
@@ -10,6 +10,7 @@ const router = Router();
 //hanya admin yg bisa lihat user
 router.get("/", authMiddleware, authorizeRoles("Admin"), asyncHandler(userController.getAllUsers));
 
+
 // register & login (tidak butuh auth)
 router.post("/register", userController.register);
 router.post("/login", userController.login);
@@ -18,9 +19,10 @@ router.post("/login", userController.login);
 router.get("/profile", authMiddleware, userController.getProfile);
 
 // GET, UPDATE, and DELETE by ID
-router.get("/:id", userController.getUserById);
-router.put("/:id", userController.updateUser);
-router.delete("/:id", userController.deleteUser);
+router.get("/:id", authMiddleware, authorizeRoles("Admin"), asyncHandler(userController.getUserById));
+
+router.put("/:id", authMiddleware, asyncHandler(userController.updateUser));
+router.delete("/:id", authMiddleware, authorizeRoles("Admin"), asyncHandler(userController.deleteUser));
 
 
 export default router;

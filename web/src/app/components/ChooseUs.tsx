5ede7f3c1467a9978de8897ae7997d94ae14ef9f
Fadly Mustofainal Ahyar <fadlymustofa0@gmail.com>
2025-11-10 13:40:54 +0700

    menghapus code error pada ChooseUs

--- web/src/app/components/ChooseUs.tsx
+++ web/src/app/components/ChooseUs.tsx
@@ -4,7 +4,6 @@ export default function ChooseUs() {
   return (
     <section className="relative bg-gradient-to-b from-[#f8fafc] to-white py-20 overflow-hidden">
               {/* Konten kanan */}
-        <div>
           <h2 className="text-center text-3xl md:text-4xl font-extrabold text-gray-900 mb-3">
             Mengapa <span className="text-blue-600">Memilih</span> Kami
           </h2>
@@ -26,7 +25,7 @@ export default function ChooseUs() {
 
         {/* Konten kanan */}
         <div>
-          <h2 className="text-3xl md:text-4xl font-extrabold text-gray-900 mb-3">
+          <h2 className="text-center md:text-4xl font-extrabold text-gray-900 mb-3">
             Mengapa <span className="text-blue-600">Memilih</span> Kami
           </h2>
           <p className="text-gray-500 mb-10">
